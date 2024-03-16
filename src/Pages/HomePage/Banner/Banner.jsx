cefa8513a30df8f942fba5a61f1e2ab67f06533a
selimnill <mrrafi812345@gmail.com>
2024-03-16 06:53:25 +0600

    ds

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -13,44 +13,44 @@ const Banner = () => {
       <div id="slide1" className="carousel-item relative w-full">
         <img src={img1} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide4" className="btn btn-circle bg-orange-600 f text-white border-orange-700 ">
-            ❮
+          <a href="#slide4" className="btn btn-circle bg-orange-600 w-14  text-white border-orange-600 ">
+            <img src={spoon1} alt="" />
           </a>
-          <a href="#slide2" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❯
+          <a href="#slide2" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-600">
+            <img src={spoon2} alt="" />
           </a>
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
         <img src={img2} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❮
+          <a href="#slide1" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-600">
+           <img src={spoon1} alt="" />
           </a>
-          <a href="#slide3" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❯
+          <a href="#slide3" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-600">
+          <img src={spoon2} alt="" />
           </a>
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
         <img src={img3} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide2" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❮
+          <a href="#slide2" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-600">
+           <img src={spoon1} alt="" />
           </a>
-          <a href="#slide4" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❯
+          <a href="#slide4" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-600">
+            <img src={spoon2} alt="" />
           </a>
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
         <img src={img4} className="w-full" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide3" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❮
+          <a href="#slide3" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-700">
+           <img src={spoon1} alt="" />
           </a>
-          <a href="#slide1" className="btn btn-circle bg-orange-600 f text-white border-orange-700">
-            ❯
+          <a href="#slide1" className="btn btn-circle bg-orange-600 w-14 text-white border-orange-700">
+            <img src={spoon2} alt="" />
           </a>
         </div>
       </div>
